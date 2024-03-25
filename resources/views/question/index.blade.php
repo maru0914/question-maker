189a265672c5a991b9d55c866186a1e605c47f68
emmaru <littleoduozu@gmail.com>
2024-03-25 22:42:32 +0900

    一覧表示のデザイン修正

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -8,34 +8,38 @@
         </a>
     </div>
 
-    <ul>
-        @foreach($questions as $question)
-            <div class="flex justify-between mb-1">
-                <li class="text-blue-600">
-                    <a href="{{ route('questions.show', ['question' => $question->id]) }}">
-                        {{ $question->body }}
-                    </a>
-                </li>
-                <span>
-                    <span class="text-gray-500">
-                        <a href="{{route('questions.edit', ['question' => $question->id])}}">
-                        編集
-                        </a>
-                    </span>
-                    <span class="text-red-500">
-                        <form method="POST" action="{{ route('questions.destroy', ['question' => $question->id]) }}">
+    <div class="bg-white shadow rounded-lg px-6">
+        <ul role="list" class="divide-y divide-gray-100">
+            @foreach($questions as $question)
+                <li class="flex justify-between items-center gap-x-6 py-5">
+                    <div class="flex min-w-0 gap-x-4">
+                        <div class="min-w-0 flex-auto text-blue-600">
+                            <a href="{{ route('questions.show', ['question' => $question->id]) }}">
+                                {{ $question->body }}
+                            </a>
+                        </div>
+                    </div>
+                    <div class="shrink-0 flex flex-col items-end">
+                        <p class="text-sm leading-6 text-gray-600">
+                            <a href="{{route('questions.edit', ['question' => $question->id])}}">
+                                編集
+                            </a>
+                        </p>
+                        <form class="text-sm leading-6 text-red-500"
+                              method="POST"
+                              action="{{ route('questions.destroy', ['question' => $question->id]) }}"
+                        >
                             @csrf
                             @method('delete')
 
-                            <button>
-                            削除
+                            <button type="submit" onclick="return confirm('本当に削除しますか？')">
+                                削除
                             </button>
                         </form>
-                    </span>
-                </span>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 
-            </div>
-
-        @endforeach
-    </ul>
 </x-app-layout>
