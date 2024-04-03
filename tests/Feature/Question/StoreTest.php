c792c241ca2012d85a9fe19ecc406c7fcc58c5f9
qroku <eabooobe@gmail.com>
2024-04-03 09:55:40 +0900

    beforeEachを追加

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -2,32 +2,29 @@
 
 use App\Models\Question;
 
-test('問題を登録できる',function () {
-    $request_question = [
+beforeEach(function() {
+    $this->request_data = [
         'body' => '問題を登録する',
         'answer' => '答え'
     ];
+});
 
-    $this->post('/questions',$request_question);
+test('問題を登録できる',function () {
+    $this->post('/questions',$this->request_data);
 
     $question = Question::where('id',1)->get()->first();
-    expect($question->body)->toBe($request_question['body']);
-    expect($question->answer)->toBe($request_question['answer']);
+    expect($question->body)->toBe($this->request_data['body']);
+    expect($question->answer)->toBe($this->request_data['answer']);
 });
 
 test('問題の登録が成功すると/questionsにリダイレクトする', function (){
-    $request_question = [
-        'body' => '問題を登録する',
-        'answer' => '答え'
-    ];
-
-    $response = $this->post('/questions',$request_question);
+    $response = $this->post('/questions',$this->request_data);
 
     $response->assertRedirect('/questions');
 });
 
-test('問題の登録が失敗すると/にリダイレクトする???', function (){
+test('問題の登録が失敗すると/questions/createにリダイレクトする', function (){
     $response = $this->post('/questions',[]);
 
-    $response->assertRedirect('/');
-});
+    $response->assertRedirect('/questions/create');
+})->todo();
