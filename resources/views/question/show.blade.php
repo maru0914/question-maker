339955a600b2c5d668af9bc77a5a4d1f5bf886dd
emmaru <littleoduozu@gmail.com>
2024-04-06 18:04:15 +0900

    答えの表示切替を実装

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -25,8 +25,8 @@
     </div>
     <div class="space-y-5 mb-5">
         <div>
-            <h2 class="font-bold text-xl mb-2">問題文</h2>
-            <div class="bg-white shadow rounded-lg p-6">
+            <h2 class="font-bold text-xl ">問題文</h2>
+            <div class="bg-white shadow rounded-lg mt-2 p-6">
                 <div class="flex min-w-0 gap-x-4">
                     <div class="min-w-0 flex-auto ">
                         {{ $question->body }}
@@ -35,15 +35,22 @@
             </div>
         </div>
 
-        {{-- TODO jsで表示/非表示切り替え　--}}
-        <div>
-            <h2 class="font-bold text-xl mb-2">答え</h2>
-            <div class="bg-white shadow rounded-lg p-6">
-                <div class="flex min-w-0 gap-x-4">
-                    <div class="min-w-0 flex-auto ">
-                        {!! nl2br(e($question->answer)) !!}
-                    </div>
-                </div>
+        <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+            <div class="flex items-center">
+                <h2 class="font-bold text-xl mr-5">答え</h2>
+                <button @click="open = ! open" class="bg-white p-1 rounded-xl">表示切替</button>
+            </div>
+
+            <div x-show="open"
+                 x-transition:enter="transition ease-out duration-200"
+                 x-transition:enter-start="opacity-0 scale-95"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-75"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-95"
+                 class="mt-2 bg-white shadow rounded-lg p-6"
+                 style="display: none;">
+                {!! nl2br(e($question->answer)) !!}
             </div>
         </div>
     </div>
