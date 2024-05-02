d60870a0d8bf8a5c0c80cc123c35eb8264a8e550
emmaru <littleoduozu@gmail.com>
2024-05-02 17:20:09 +0900

    問題集用のページを追加

---
+++ resources/views/book/edit.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $book->title }} - 編集
+        </h2>
+    </x-slot>
+    <form method="POST" action="{{ route('books.update', ['book' => $book->id]) }}" enctype="multipart/form-data">
+        @csrf
+        @method('patch')
+        <div class="grid sm:grid-cols-2 gap-2">
+            <div class="flex flex-col justify-center">
+                <x-input-label for="title" value="タイトル"/>
+                <x-text-input id="title" class="block mt-1 w-full" name="title" :value="old('title') ?? $book->title"
+                              required autofocus/>
+                <x-input-error :messages="$errors->get('title')" class="mt-2"/>
+            </div>
+            <div class="flex flex-col justify-center">
+                <x-input-label for="image" value="画像"/>
+                <x-input-file name="image" alt="books" :src="asset('storage/' . $book->image_path)" />
+                <x-input-error :messages="$errors->get('image')" class="mt-2"/>
+            </div>
+        </div>
+        <div class="mt-4">
+            <x-input-label for="description" value="説明文"/>
+            <x-text-area id="description" class="block mt-1 w-full" name="description" rows="5"
+                         :value="old('description')"
+                         required autofocus>
+                {{ old('description') ?? $book->description }}
+            </x-text-area>
+            <x-input-error :messages="$errors->get('description')" class="mt-2"/>
+        </div>
+
+        <div class="flex items-center justify-between mt-4">
+            <a class="text-blue-600" href="{{ route('books.index') }}">
+                {{ __('Books') }}に戻る
+            </a>
+            <x-primary-button>
+                更新
+            </x-primary-button>
+        </div>
+    </form>
+</x-app-layout>
---
+++ resources/views/components/input-file.blade.php
@@ -0,0 +1,49 @@
+@props([
+    'name' => 'image',
+    'src' => '',
+    'alt' => '',
+])
+
+<script>
+    function imageViewer(src = '') {
+        return {
+            imageUrl: src,
+
+            fileChosen(event) {
+                this.fileToDataUrl(event, src => this.imageUrl = src)
+            },
+
+            fileToDataUrl(event, callback) {
+                if (!event.target.files.length) return
+
+                let file = event.target.files[0],
+                    reader = new FileReader()
+
+                reader.readAsDataURL(file)
+                reader.onload = e => callback(e.target.result)
+            },
+        }
+    }
+</script>
+<div x-data="imageViewer('{{ $src }}')">
+    <template x-if="imageUrl">
+        <img :src="imageUrl"
+             class="object-cover rounded border border-gray-200"
+             style="width: 100px; height: 100px;"
+             alt="{{ $alt }}"
+        >
+    </template>
+    <template x-if="!imageUrl">
+        <div
+                class="border rounded border-gray-200 bg-gray-100"
+                style="width: 100px; height: 100px;"
+        ></div>
+    </template>
+
+    <div class="flex flex-col lg:flex-row">
+        <div>
+            <input class="mt-2" name="{{ $name }}" type="file" accept="image/*" @change="fileChosen">
+        </div>
+    </div>
+
+</div>
