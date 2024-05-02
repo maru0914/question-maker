6929e1853fc8f6b95b956145a47e2d9306bf21d2
emmaru <littleoduozu@gmail.com>
2024-05-02 17:40:27 +0900

    多言語対応を追加

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            問題一覧
+            {{ __('Questions') }}
         </h2>
     </x-slot>
     @auth
