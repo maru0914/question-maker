febf1becf5f28ab85a8be5e191b147e74cd03e6b
emmaru <littleoduozu@gmail.com>
2024-05-15 11:00:08 +0900

    テストコードの修正

--- tests/Feature/Question/IndexTest.php
+++ tests/Feature/Question/IndexTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\QuestionResource;
 use App\Models\Question;
 
 test('問題が登録されていない場合リストが表示されない', function () {
@@ -8,15 +9,13 @@ test('問題が登録されていない場合リストが表示されない', fu
         ->assertDontSee('編集');
 });
 
-test('問題が登録されている場合リストが表示される', function () {
-    $question = Question::factory()->create();
+test('questionsがコンポーネントに渡される', function () {
+    $questions = Question::factory(3)->create();
+
+    $questions->load('user');
 
     $this->get('/questions')
-        ->assertOK()
-        ->assertViewHas('questions', function ($questions) {
-            return count($questions) === 1;
-        })
-        ->assertSee($question->body);
+        ->assertHasPaginatedResource('questions', QuestionResource::collection($questions->reverse()));
 });
 
 test('クエリパラメータpageが数値以外の場合1ページ目の内容が表示される', function () {
@@ -34,42 +33,4 @@ test('作成の新しい順に問題が表示される', function () {
     $this->get('/questions')
         ->assertOK()
         ->assertSeeInOrder([$question2->body, $question1->body]);
-});
-
-describe('ページネーションのテスト', function () {
-    test('登録されている問題が20件以下の場合ページネーションが表示されない', function () {
-        Question::factory()->count(20)->create();
-
-        $this->get('/questions')
-            ->assertOK()
-            ->assertViewHas('questions', function ($questions) {
-                return count($questions) === 20;
-            })
-            ->assertDontSee('/questions?page=');
-    });
-
-    test('登録されている問題が21件以上の場合ページネーションが表示される', function () {
-        Question::factory()->count(21)->create();
-
-        $this->get('/questions')
-            ->assertOK()
-            ->assertViewHas('questions', function ($questions) {
-                return count($questions) === 20;
-            })
-            ->assertSee('/questions?page=2');
-    });
-
-    test('2ページ目以降にいる場合前のページへのリンクが表示される', function () {
-        Question::factory()->count(21)->create();
-
-        $this->get('/questions?page=2')
-            ->assertOK()
-            ->assertSee('/questions?page=1');
-    });
-
-    test('存在しないページ数にアクセスした場合も前のページへのリンクが表示される', function () {
-        $this->get('/questions?page=2')
-            ->assertOk()
-            ->assertSee('/questions?page=1');
-    });
 });
