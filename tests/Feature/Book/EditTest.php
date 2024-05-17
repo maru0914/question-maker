48287d884d1e90241c9233e3e620b4678295d6a7
maru0914 <littleoduozu@gmail.com>
2024-05-17 16:20:15 +0900

    テストコードの修正

--- tests/Feature/Book/EditTest.php
+++ tests/Feature/Book/EditTest.php
@@ -1,39 +1,37 @@
 <?php
 
-use App\Models\Question;
+use App\Models\Book;
 use App\Models\User;
 
 beforeEach(function () {
     $this->user = User::factory()->create();
-    $this->question = Question::factory()->for($this->user)->create();
+    $this->book = Book::factory()->for($this->user)->create();
 });
 
-test('保存されている内容を閲覧できる', function () {
+test('編集画面にアクセスできる', function () {
     $this->actingAs($this->user)
-        ->get("/questions/{$this->question->id}/edit")
-        ->assertOk()
-        ->assertSee($this->question->body)
-        ->assertSee($this->question->answer);
+        ->get("/books/{$this->book->id}/edit")
+        ->assertOk();
 });
 
 test('他ユーザーが作成した問題の編集画面はアクセスできない', function () {
-    $questionByOtherUser = Question::factory()->create();
+    $bookByOtherUser = Book::factory()->create();
 
     $this->actingAs($this->user)
-        ->get("/questions/{$questionByOtherUser->id}/edit")
+        ->get("/books/{$bookByOtherUser->id}/edit")
         ->assertForbidden();
 });
 
 test('idが存在しない場合、404を返す', function () {
-    $latestQuestion = Question::orderBy('id', 'desc')->first();
-    $nonExistingId = $latestQuestion->id + 1;
+    $latestBook = Book::orderBy('id', 'desc')->first();
+    $nonExistingId = $latestBook->id + 1;
 
     $this->actingAs($this->user)
-        ->get("/questions/{$nonExistingId}/edit")
+        ->get("/books/{$nonExistingId}/edit")
         ->assertNotFound();
 });
 
 test('ログインしていない場合ログインページにリダイレクト', function () {
-    $this->get("/questions/{$this->question->id}/edit")
+    $this->get("/books/{$this->book->id}/edit")
         ->assertRedirect('/login');
 });
