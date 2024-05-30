716540df990d95cb23d9e87c7390c1eb0f947e27
maru0914 <littleoduozu@gmail.com>
2024-05-30 17:21:44 +0900

    Laravel Pulseの導入

---
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -0,0 +1,13 @@
+<x-pulse>
+    <livewire:pulse.servers cols="full" />
+
+    <livewire:pulse.usage cols="6" rows="2" />
+
+    <livewire:pulse.slow-queries cols="6" />
+
+    <livewire:pulse.exceptions cols="6" />
+
+    <livewire:pulse.slow-requests cols="6" />
+
+    <livewire:pulse.slow-outgoing-requests cols="6" />
+</x-pulse>
