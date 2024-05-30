f145de3b64b9379d66cf8536843a83058fa5fa2e
maru0914 <littleoduozu@gmail.com>
2024-05-30 17:48:43 +0900

    Pulse関連の設定をAppServiceProviderに移動

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pulse\Facades\Pulse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->is_admin;
+        });
+
+        Pulse::user(fn ($user) => [
+            'name' => $user->username,
+            'extra' => $user->email,
+            'avatar' => sprintf('https://gravatar.com/avatar/%s?d=mp', hash('sha256', trim(strtolower($user['email'])))),
+        ]);
     }
 }
