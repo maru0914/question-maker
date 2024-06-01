8cef813fd7982eab1109c4932141c5ec1c0f93ad
emmaru <littleoduozu@gmail.com>
2024-06-01 14:13:37 +0900

    問題集一覧ページのデザイン修正

--- resources/js/Components/CreateIcon.vue
+++ resources/js/Components/CreateIcon.vue
@@ -1,6 +1,8 @@
 <template>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-             class="w-6 h-6 text-blue-500">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+    <div class="bg-indigo-500/90 text-white p-3 rounded-full shadow-lg hover:bg-indigo-600 transition">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+             xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
         </svg>
+    </div>
 </template>
--- resources/js/Pages/Book/Index.vue
+++ resources/js/Pages/Book/Index.vue
@@ -6,42 +6,47 @@ import Panel from "@/Components/Panel.vue";
 import Pagination from "@/Components/Pagination.vue";
 import {computed} from "vue";
 
-
 const props = defineProps({
     books: {
         type: Object,
-        required: true
+        required: true,
     },
 });
 
 const hasMultiplePages = computed(() => props.books.meta.total > props.books.meta.per_page);
-
 </script>
 
 <template>
     <Head title="問題集一覧"/>
     <AppLayout>
         <template #header>問題集一覧</template>
-        <template #subtitle>好きな問題集を選んで、知識を身につけましょう！
+        <template #subtitle>
+            好きな問題集を選んで、知識を身につけましょう！
             <p v-if="!$page.props.auth.user" class="mt-2 text-sm">
-                ※<Link class="text-indigo-500" :href="route('login')">ログイン</Link>すると問題集の作成や解いた問題の正誤を記録できます!
+                ※
+                <Link class="text-indigo-500" :href="route('login')">ログイン</Link>
+                すると問題集の作成や解いた問題の正誤を記録できます!
             </p>
         </template>
 
-        <div v-if="$page.props.auth.user" class="flex justify-end mb-2">
+        <div v-if="$page.props.auth.user" class="flex justify-end mb-4">
             <Link :href="route('books.create')">
-                <CreateIcon />
+                <CreateIcon/>
             </Link>
         </div>
 
-        <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
-            <Panel v-for="book in books.data" :key="book.id" class="relative border border-gray-200 rounded-lg hover:scale-105 hover:shadow-lg transition-transform duration-300 ease-in-out">
+        <div class="grid grid-cols-2 sm:grid-cols-4 gap-6">
+            <Panel
+                v-for="book in books.data"
+                :key="book.id"
+                class="relative border border-gray-200 rounded-lg hover:scale-105 hover:shadow-xl transition-transform duration-300 ease-in-out"
+            >
                 <Link :href="route('books.show', book.id)" class="absolute inset-0"></Link>
                 <div class="flex flex-col w-full h-full p-4">
                     <div class="flex items-center justify-center mb-4">
-                        <img class="max-h-40" :src="'/storage/' + book.image_path" alt="">
+                        <img class="max-h-40" :src="'/storage/' + book.image_path" alt=""/>
                     </div>
-                    <h2 class="text-lg text-center mb-2">{{ book.title }}</h2>
+                    <h2 class="text-lg text-center mb-2 font-semibold">{{ book.title }}</h2>
                     <div class="text-gray-500 text-xs text-right">
                         by {{ book.user.username }}
                     </div>
